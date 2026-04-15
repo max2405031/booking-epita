0c19aa93e85b9912ab55117a28883ed565a2c49d
brunet <brunet@epita.fr>
2026-04-15 12:52:16 +0200

    feat: credit card number masking

--- src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/persistence/CreditCardConverter.java
+++ src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/persistence/CreditCardConverter.java
@@ -37,9 +37,13 @@ public class CreditCardConverter implements AttributeConverter<String, String> {
     }
 
     private String panMasking(String pan) {
-        // Step 6:
-        return pan;
-        // Step 6: End
+        if (pan == null || pan.length() < 8) {
+            return pan;
+        }
+        String firstFour = pan.substring(0, 4);
+        String lastFour = pan.substring(pan.length() - 4);
+        String masked = "*".repeat(pan.length() - 8);
+        return firstFour + masked + lastFour;
     }
 
     
